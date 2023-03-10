c5cbaabaf0488dcaead8190dd12f869c86e1f4ea
lothin9999 <lothin9999@gmail.com>
2023-03-10 16:57:38 +0700

    sidebarmenu

--- components/TopSideBar.vue
+++ components/TopSideBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar toggleable="md" type="dark" variant="light" class="shadow">
       <b-icon v-b-toggle.sidebar-1 class="h2" icon="text-center" style="color: #7952b3;"></b-icon>
-      <b-navbar-brand class="ml-4 h2">Nuxt LaRa People</b-navbar-brand>
+      <b-navbar-brand class="ml-4 h2 text-primary">Nuxt LaRa People</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -11,20 +11,12 @@
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
             <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            <b-button size="sm" class="my-2 my-sm-0 text-" type="submit">Search</b-button>
           </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
           <b-nav-item-dropdown right>
             <!-- Using 'button-content' slot -->
             <template #button-content>
-              <em>User</em>
+              <h6 class="text-primary mt-3">User</h6>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
             <b-dropdown-item href="#">Sign Out</b-dropdown-item>
